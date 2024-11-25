161af7506094db8fc74290f5217a519934cf9ebf
Rodolfo Sousa <naodocover@gmail.com>
2024-11-25 17:15:49 -0300

    Update

--- funcbib01.py
+++ funcbib01.py
@@ -1,7 +1,6 @@
 ##importando biblitecas
 import datetime as data
 import time as tempo
-
 ## Criando Dicionarios de Dados com Listas por Cadastro
 dicionarioBiblioteca = {
     "autores": [],
@@ -9,20 +8,15 @@ dicionarioBiblioteca = {
     "alunos": [],
     "emprestimos": []
 }
-
-
 ## DEFININDO FUNÇÃO PARA ENFEITE
 def mostraLinha():
     print("-" * 60)
-
-
 ## Definindo Função para Cadastro de Livro na lista 
 def cadastraLivro(id, titulo, autor, dataCadastro=None, dataAtualizacao=None, disponivel=True):
     if dataCadastro is None:
         dataCadastro = data.datetime.now().strftime("%d/%m/%y") ## coloca a data atual e o strftime coloca como string a data
     if dataAtualizacao is None:
         dataAtualizacao = data.datetime.now().strftime("%d/%m/%y") ## coloca a data atual e o strftime coloca como string a data
-    
     addlivro = {
         "id": id,
         "titulo": titulo,
@@ -32,8 +26,6 @@ def cadastraLivro(id, titulo, autor, dataCadastro=None, dataAtualizacao=None, di
         "disponivel": disponivel
     }
     dicionarioBiblioteca["livros"].append(addlivro)
-
-
 ## Definindo Função que Cadastra o Autor
 def cadastraAutor(id, nome, dataNascimento):
     addAutor = {
@@ -42,8 +34,6 @@ def cadastraAutor(id, nome, dataNascimento):
         "dataNascimento": dataNascimento
     }
     dicionarioBiblioteca["autores"].append(addAutor)
-
-
 ## Definindo Função que cadastra o aluno
 def cadastraAluno(id, nome, dataNascimento):
     addAluno = {
@@ -52,21 +42,15 @@ def cadastraAluno(id, nome, dataNascimento):
         "dataNascimento": dataNascimento
     }
     dicionarioBiblioteca["alunos"].append(addAluno)
-
-
 ## Definindo função de busca livro por titulo
 def buscaLivrotit(titulo):
     return [livro for livro in dicionarioBiblioteca["livros"]
             if titulo.lower() in livro["titulo"].lower()]
-
-
 ## Definindo Função de disponibilidade de livros
 def livrosDisp():
     for livro in dicionarioBiblioteca["livros"]:
         if livro["disponivel"]:
             print(f"O livro {livro['id']} {livro['titulo']} disponível desde {livro['dataAtualizacao']}")
-
-
 ## Definindo função de emprestimo de livros
 def emprestimoLivro(livroId, alunoId):
     livroEncontrado = next((livro for livro in dicionarioBiblioteca["livros"] if livro["id"] == livroId and livro["disponivel"]), None) ##resumindo, faz a busca livro por livro no dicionario
@@ -77,7 +61,7 @@ def emprestimoLivro(livroId, alunoId):
             livroEncontrado["dataAtualizacao"] =  data.datetime.now().strftime("%d/%m/%y") ## atualizad a data de atualização convertendo para string ja que o datetime retorna valor inteiro
             print(f"Livro {livroEncontrado['titulo']} emprestado ao aluno {alunoEncontrado['nome']}.")
         else:
-            print("⚠️AVISO! ALUNO NÃO CADASTRADO")
+            print("⚠️ AVISO! ALUNO NÃO CADASTRADO")
             id = int(input("Digite o ID do aluno: "))
             nome = str(input("Nome do Aluno: "))
             dataNascimento = str(input("Data de Nascimento: "))
@@ -85,32 +69,24 @@ def emprestimoLivro(livroId, alunoId):
             print("Aluno cadastrado com sucesso.")
     else:
         print("Livro não disponível ou não encontrado.")
-
-
 ## Definindo função de busca livro por id
 def buscaLivroid(id):
     return [livro for livro in dicionarioBiblioteca["livros"] if livro["id"] == id]
-
-
 ## Definindo Função de busca aluno
 def buscaAlunos(nome):
     return [aluno for aluno in dicionarioBiblioteca["alunos"] if nome.lower() in aluno["nome"].lower()]
-
-
 def devolverLivro(livroId, alunoId):
     livroEncontrado = next((livro for livro in dicionarioBiblioteca["livros"] if livro["id"] == livroId and not livro["disponivel"]), None)
-    
     if livroEncontrado:
         alunoEncontrado = next((aluno for aluno in dicionarioBiblioteca["alunos"] if aluno["id"] == alunoId), None)
         if alunoEncontrado:
             livroEncontrado["disponivel"] = True
             livroEncontrado["dataAtualizacao"] = data.datetime.now().strftime("%d/%m/%y")
-            print(f"Livro {livroEncontrado['titulo']} devolvido pelo aluno {alunoEncontrado['nome']}.")
+            print(f"Livro {livroEncontrado['titulo']} Entregue pelo aluno(a) {alunoEncontrado['nome']} no dia {livroEncontrado["dataAtualizacao"]}")
         else:
-            print("⚠️AVISO! ALUNO NÃO CADASTRADO")
+            print("⚠️ AVISO! ALUNO NÃO CADASTRADO")
     else:
-        print("Livro não encontrado ou já está disponível.")    
-
+        print(" ⚠️ Livro não encontrado ")    
 ## INICIO DO MENU DEFININDO A FUNÇÃO 
 def menuInicial():
     while True:
@@ -126,11 +102,9 @@ def menuInicial():
                   [8] - Devolução de Livros
                   [0] - Sair
                """)
-        
         mostraLinha()
         escolha = int(input("Digite a numeração de uma das opções acima: "))
         mostraLinha()
-
         if escolha == 1:
             mostraLinha()
             print("Para cadastrar um autor, por favor preencher os campos abaixo")
@@ -140,7 +114,6 @@ def menuInicial():
             cadastraAutor(id, nome, dataNascimento)
             print("Cadastrado com Sucesso")
             mostraLinha()
-
         elif escolha == 2:
             mostraLinha()
             print("Para cadastrar o aluno, preencher os campos abaixo")
@@ -150,7 +123,6 @@ def menuInicial():
             cadastraAluno(id, nome, dataNascimento)
             print("Cadastrado com Sucesso")
             mostraLinha()
-
         elif escolha == 3:
             mostraLinha()
             print("Para cadastrar um livro, siga os dados necessários")
@@ -161,17 +133,14 @@ def menuInicial():
             cadastraLivro(id, titulo, autor)
             print("Cadastrado com Sucesso")
             mostraLinha()
-
         elif escolha == 4:
             mostraLinha()
             print(dicionarioBiblioteca["autores"])
             mostraLinha()
-
         elif escolha == 5:
             mostraLinha()
             print(dicionarioBiblioteca["alunos"])
             mostraLinha()
-
         elif escolha == 6:
             mostraLinha()
             livrosDisp()
@@ -179,20 +148,16 @@ def menuInicial():
             alunoId = int(input("Digite o ID do aluno: "))
             emprestimoLivro(livroId, alunoId)
             mostraLinha()
-
         elif escolha == 7:
             mostraLinha()
             livrosDisp()
             mostraLinha()
-
         elif escolha == 8:
             mostraLinha()
             livroId = int(input("Digite o ID do livro: "))
             alunoId = int(input("Digite o ID do aluno: "))
             devolverLivro(livroId, alunoId)
             mostraLinha()
-
-
         elif escolha == 0:
             print("Sistema está se encerrando...⏳")
             for i in range(5, -1, -1):
@@ -200,8 +165,7 @@ def menuInicial():
                 tempo.sleep(1)
             print("Sistema se encerrou!✅")
             break
-
         # SIMPLES, SE NAO FOR UMA DAS OPÇÕES DO MENU, APARECE O PRINT AVISANDO
         elif escolha not in [0, 1, 2, 3, 4, 5, 6, 7]:
             print("Opção inválida. Tente novamente.")
-## AVALIE CM CARINHO PROFESSORA
+## AVALIE COM CARINHO PROFESSORA
